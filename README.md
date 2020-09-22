6c6a77fe9bbbe69e8a25d54d6738e3422d16b216
Isaac <isaackomeza@gmail.com>
2020-09-22 12:23:21 +0200

    coveralls and coverage scripts have been added

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # orcas-phantom-backend
-Phantom is a web application that allows passengers to track bus location and movements around Kigali
+[![Build Status](https://travis-ci.org/atlp-rwanda/orcas-phantom-backend.svg?branch=ch-traviscov-backend)](https://travis-ci.org/atlp-rwanda/orcas-phantom-backend)
+[![Coverage Status](https://coveralls.io/repos/github/atlp-rwanda/orcas-phantom-backend/badge.svg?branch=ch-traviscov-backend)](https://coveralls.io/github/atlp-rwanda/orcas-phantom-backend?branch=ch-traviscov-backend)
---
+++ src/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../swagger.json';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/swaggerDocument', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.get('/', (req, res) => res.status(200).json({
+  message: 'Welcome to orcas-phanthom backend site',
+}));
+
+app.use((req, res) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  res.send({ status: error.status, message: error.message });
+});
+
+export default app;
