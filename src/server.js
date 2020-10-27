394716e3787ba07b922479171a1c999cf9a89e4f
Isaac <isaackomeza@gmail.com>
2020-10-27 09:23:59 +0200

    ft(cors):bug fix on cors implementation

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import app from './index';
 import db from './database/models';
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 9000;
 db.sequelize.sync({ alter: false })
   .then(() => {
     console.log('database connected.');
